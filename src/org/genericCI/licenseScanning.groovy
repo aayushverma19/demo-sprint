9612f242179fbc8f69d7a159f84a7f809f2c9983
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-22 23:38:42 +0530

    Update licenseScanning.groovy

--- src/org/genericCI/licenseScanning.groovy
+++ src/org/genericCI/licenseScanning.groovy
@@ -1,3 +1,4 @@
+package org.genericCI
 def call(String project_api_key) {
   stage ('License Scanning') {
         withCredentials([string(credentialsId: 'project_api_key', variable: 'FOSSA_KEY')]) { 
