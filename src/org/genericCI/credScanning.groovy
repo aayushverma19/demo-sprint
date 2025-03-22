c3e06f102b8ca5d0a159dea8ad8b1a474eaabf7f
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-23 01:10:33 +0530

    Update credScanning.groovy

--- src/org/genericCI/credScanning.groovy
+++ src/org/genericCI/credScanning.groovy
@@ -2,6 +2,7 @@ package org.genericCI
 
 def call(String reportName) {
     stage ('Cred Scanning'){
+        sh 'mkdir allreports'
         sh "gitleaks detect -f json -r allreports/${reportName}.json"
     }
    // archiveArtifacts artifacts: reportName, fingerprint: true
