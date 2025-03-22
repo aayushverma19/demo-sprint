d60dc95d241ed12d381d4022a18b5c4388e21edc
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-22 23:38:05 +0530

    Create licenseScanning.groovy

---
+++ src/org/genericCI/licenseScanning.groovy
@@ -0,0 +1,11 @@
+def call(String project_api_key) {
+  stage ('License Scanning') {
+        withCredentials([string(credentialsId: 'project_api_key', variable: 'FOSSA_KEY')]) { 
+          sh """
+            export FOSSA_API_KEY=${FOSSA_KEY}
+            fossa analyze
+            fossa report --format html attribution >${reportFile}
+          """
+        }
+    }
+}
