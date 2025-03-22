79377764069afab37fd996f5709f10716580cff9
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-23 00:21:43 +0530

    Update templatepipeline.groovy

--- src/org/template/templatepipeline.groovy
+++ src/org/template/templatepipeline.groovy
@@ -1,17 +1,20 @@
 package org.template
 
 import org.common.*
+import org.genericCI.*
 import org.Java.* 
 //import org.Java.codeCompile
 
 def call (String branch, String repoUrl, String gitPassword){
   clean = new cleanWS()
   clone = new gitclone()
+  gitleaks = new credScanning()
   compile = new codeCompile()
   unit = new unitTesting()
 
   clean.call()
   clone.call(branch, repoUrl, gitPassword)
+  gitleaks.call()
   compile.call()
   unit.call()
 
