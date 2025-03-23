5edc0d853f583a2b7d3a3de766c89d482760fb05
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-23 19:27:59 +0530

    Update staticCode.groovy

--- src/org/java/staticCode.groovy
+++ src/org/java/staticCode.groovy
@@ -1,1 +1,13 @@
 package org.java
+def call(String projectKey, String credentialsId ) {
+  stage ('Static Code Analysis')
+    withSonarQubeEnv('sonar') { 
+        withCredentials([string(credentialsId: credentialsId, variable: 'SONARQUBE_TOKEN')]) {
+            sh """
+            mvn clean verify sonar:sonar \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.token=${SONARQUBE_TOKEN}
+            """
+        }
+    }
+}
