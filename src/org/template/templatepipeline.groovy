7d6c98c288c47affdc42f0ac6af3dbb5bbcbdee2
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-24 01:38:36 +0530

    Update templatepipeline.groovy

--- src/org/template/templatepipeline.groovy
+++ src/org/template/templatepipeline.groovy
@@ -5,7 +5,7 @@ import org.genericCI.*
 import org.java.* 
 //import org.Java.codeCompile
 
-def call (String reportName, String branch, String repoUrl, String gitPassword, String bugprojectKey, String sonar_token, String staticprojectKey){
+def call (String reportName, String branch, String repoUrl, String gitPassword, String bugprojectKey, String sonar_token, String staticprojectKey, String zapUrl, String zapReport){
 //def call (String reportName, String branch, String repoUrl, String gitPassword, String zapUrl, String zapReport, 
  //         String bugprojectKey, String sonar_token, String staticprojectKey, String project_api_key, String reportFile){
           
@@ -30,7 +30,7 @@ def call (String reportName, String branch, String repoUrl, String gitPassword, 
   unit.call()
   //dependency.call()
   staticode.call(staticprojectKey, sonar_token)
-  //DASTcheck.call(zapUrl, zapReport)
+  DASTcheck.call(zapUrl, zapReport)
   
 
 }
