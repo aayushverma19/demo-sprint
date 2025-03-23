d536631e282df1618dfef3a58893ea286db48b45
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-23 19:32:36 +0530

    Update BugsAnalysis.groovy

--- src/org/java/BugsAnalysis.groovy
+++ src/org/java/BugsAnalysis.groovy
@@ -1,11 +1,11 @@
 package org.java
-def call(String projectKey, String credentialsId ) {
+def call(String bugprojectKey, String credentialsId ) {
   stage ('Bugs Analysis')
     withSonarQubeEnv('sonar') { 
         withCredentials([string(credentialsId: credentialsId, variable: 'SONARQUBE_TOKEN')]) {
             sh """
             mvn clean verify sonar:sonar \
-                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectKey=${bugprojectKey} \
                     -Dsonar.token=${SONARQUBE_TOKEN}
             """
         }
