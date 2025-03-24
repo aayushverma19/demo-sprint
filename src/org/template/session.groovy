6831b984fb24b7d7bc43ec982fc7dd074b7ea615
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-24 18:36:22 +0530

    Update session.groovy

--- src/org/template/session.groovy
+++ src/org/template/session.groovy
@@ -5,6 +5,21 @@ import org.generic.*
 import org.java.*
 //import org.java.codeCompile
 
+  def call (String reportName, String branch, String repoUrl, String gitPassword ){
+
+  clean = new cleanWS()
+  clone = new gitclone()
+  gitleaks = new credScanning()
+  compile = new codeCompile()
+  unit = new unitTesting()
+
   
+  clean.call()
+  clone.call(branch, repoUrl, gitPassword)
+  gitleaks.call(reportName)
+  compile.call()
+  unit.call()
+  
+}
 
   
