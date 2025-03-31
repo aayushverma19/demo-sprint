53519a4100ed9be6e8e395fac95fee7e0b9b5bde
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-31 17:22:46 +0530

    Create deploye.groovy

---
+++ src/database/postgresql/deploye.groovy
@@ -0,0 +1,5 @@
+package database.postgresql
+
+def ancibleExecute(String inventory_path, String playbook_path, String key_path){
+  sh "ansible-playbook -i ${inventory_path} ${playbook_path} --private-key=${key_path} -u ubuntu"
+}
