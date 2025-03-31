2865ea4a18412ade242e1e3f8b37e186350b06fe
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-31 18:43:53 +0530

    Update deploy.groovy

--- src/database/postgresql/deploy.groovy
+++ src/database/postgresql/deploy.groovy
@@ -1,5 +1,6 @@
 package database.postgresql
 
 def ancibleExecute(String inventory_path, String playbook_path, String key_path){
-  sh "ansible-playbook -i ${inventory_path} ${playbook_path} --private-key=${key_path} -u ubuntu"
+ // sh "ansible-playbook -i ${inventory_path} ${playbook_path} --private-key=${key_path} -u ubuntu"
+  sh "ansible-playbook -i inventory_path playbook_path --private-key=key_path -u ubuntu"
 }
