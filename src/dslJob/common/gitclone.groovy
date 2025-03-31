10ea2ae0a1f7b0b9c5ad3a03731a064aa7389599
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-31 17:17:02 +0530

    Delete src/dslJob/common directory

--- src/dslJob/common/gitclone.groovy
+++
@@ -1,9 +0,0 @@
-package org.common
-def cloneRepo(String branch , String repoUrl, String gitPassword) {
-        stage('Clone Repo'){
-            checkout([$class: 'GitSCM',
-                branches: [[name: "*/${branch}"]],
-                userRemoteConfigs: [[url: repoUrl , credentialsId: gitPassword ]]
-              ])
-        }
-}
