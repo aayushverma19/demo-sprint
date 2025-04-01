0b652b2d6dc58e98eceb8afd4cc8d03d06d47e41
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-01 12:33:07 +0530

    Update gitclone.groovy

--- src/database/common/gitclone.groovy
+++ src/database/common/gitclone.groovy
@@ -1,9 +1,13 @@
 package org.common
 def cloneRepo(String branch , String repoUrl, String gitPassword) {
         stage('Clone Repo'){
-            checkout([$class: 'GitSCM',
-                branches: [[name: "*/${branch}"]],
-                userRemoteConfigs: [[url: repoUrl , credentialsId: gitPassword ]]
-              ])
+            script {
+                    withCredentials([string(credentialsId: "${credentials_Id}", variable: 'TOKEN')]) {
+                
+                        sh """
+                         git clone --branch ${version} --depth 1 https://${user_name}:${TOKEN}@github.com/snaatak-Zero-Downtime-Crew/demofortag.git
+                        """
+                    }
+                }
         }
 }
