8b24522359b2f0feeda847972acc043ada12d32a
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-01 12:28:20 +0530

    Update deployPostgres.groovy

--- src/database/template/deployPostgres.groovy
+++ src/database/template/deployPostgres.groovy
@@ -5,8 +5,8 @@ import database.postgresql.*
 
   def call (String branch , String repoUrl, String gitPassword, String inventory_path, String playbook_path, String key_path) {
 
-    clone =  new.gitclone()
-    deployDB = new .deploy()
+    clone =  new gitclone()
+    deployDB = new deploy()
 
     clone(branch, repoUrl, gitPassword)
     deploydb(inventory_path, playbook_path, key_path)
