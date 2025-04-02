e7858ad3e7500ae32736b542165652b6a1df39eb
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-02 18:43:18 +0530

    Update ansibleLint.groovy

--- src/org/ansibleCI/ansibleLint.groovy
+++ src/org/ansibleCI/ansibleLint.groovy
@@ -1,10 +1,10 @@
 package org.ansibleCI
 
-def call (String playbook_path){
+def call (String taskFile_path){
       stage('Ansible Lint'){
                  script {
                     catchError(buildResult: 'SUCCESS') {
-                        sh "ansible-lint ${playbook_path} > Report-ansible-lint.txt"
+                        sh "ansible-lint ${taskFile_path} > Report-ansible-lint.txt"
                  }
             }
         }
