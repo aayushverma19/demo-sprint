448f98ca83a81741577fc5caef4e728a0d0c93c4
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-03 00:36:01 +0530

    Update gitclone.groovy

--- src/database/common/gitclone.groovy
+++ src/database/common/gitclone.groovy
@@ -5,7 +5,7 @@ def call(String credentials_Id, String version, String user_name) {
                     withCredentials([string(credentialsId: "${credentials_Id}", variable: 'TOKEN')]) {
                 
                         sh """
-                         git clone --branch ${version} --depth 1 https://${user_name}:${TOKEN}@github.com/snaatak-Zero-Downtime-Crew/tagdemo.git
+                         git clone --branch ${version} --depth 1 https://${user_name}:${TOKEN}@github.com/snaatak-Zero-Downtime-Crew/ansible.git
                         """
                     }
                 }
