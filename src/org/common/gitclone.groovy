92d3f0256f07e25587a42e178731ebb77b6680c8
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-02 14:15:28 +0530

    Update gitclone.groovy

--- src/org/common/gitclone.groovy
+++ src/org/common/gitclone.groovy
@@ -3,9 +3,10 @@ package org.common
 def call(String branch_name, String repo_url, String git_password) {
 
 stage('Clone Repo') {
-              checkout([$class: 'GitSCM',
-                  branches: [[name: "*/${env.branch_name}"]],
-                  userRemoteConfigs: [[url: env.repo_url, credentialsId: env.git_password]]
-              ])
-      }
+        checkout([$class: 'GitSCM',
+                    branches: [[name: "*/${branch_name}"]],
+                    userRemoteConfigs: [[url: repo_url , credentialsId: git_password ]]
+                    ])
+          }
+
 }
