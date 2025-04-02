1c290c4a97f72946ab7b16046004d0c65ab0d11c
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-02 14:07:19 +0530

    Update dryRun.groovy

--- src/org/ansibleCI/dryRun.groovy
+++ src/org/ansibleCI/dryRun.groovy
@@ -2,11 +2,10 @@ package org.ansibleCI
 
 def call (String inventory_path, String playbook_path, String key_path, String user_name ){
   stage('Dry Run'){
-            steps {
+
                 script {
                     catchError(buildResult: 'SUCCESS') {
                         sh "ansible-playbook -C -D -i ${inventory_path} ${playbook_path} --private-key=${key_path} -u ${user_name}  > Report-dry-run.txt"
-                    }
                 }
             }
       }
