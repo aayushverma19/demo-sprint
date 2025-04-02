f512162331ae4cc3ff282410487161aba7564dc8
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-03 01:00:55 +0530

    Update deploy.groovy

--- src/database/postgresql/deploy.groovy
+++ src/database/postgresql/deploy.groovy
@@ -2,6 +2,7 @@ package database.postgresql
 
 def call(String inventory_path, String playbook_path, String key_path){
   stage ('Deploy') {
+    
     sh "ansible-playbook -i ${inventory_path} ${playbook_path} --private-key=${key_path} -u ubuntu"
    // sh "ansible-playbook -i inventory_path playbook_path --private-key=key_path -u ubuntu"
   }
