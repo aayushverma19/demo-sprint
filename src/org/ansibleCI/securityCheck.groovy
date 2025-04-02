6fa625d10ed2b5971f90ffc2d54e22cf96f3ff8b
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-02 14:41:51 +0530

    Update securityCheck.groovy

--- src/org/ansibleCI/securityCheck.groovy
+++ src/org/ansibleCI/securityCheck.groovy
@@ -4,7 +4,7 @@ def call (String playbook_path){
     stage('Security Check'){
                     script {
                         catchError(buildResult: 'SUCCESS') {
-                        sh "${HOME}.local/bin/ansible-later ${playbook_path} 2>&1 | tee Report-security.txt"
+                        sh "${HOME}/.local/bin/ansible-later ${playbook_path} 2>&1 | tee Report-security.txt"
                   }
             }
       }
