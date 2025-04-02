37c30beab416c6fd52d334c479fb12be2ca1dc0d
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-02 12:56:02 +0530

    Create ansiblelint.groovy

---
+++ src/org/ansibleCI/ansiblelint.groovy
@@ -0,0 +1,11 @@
+def call (String playbook_path){
+      stage('Ansible Lint'){
+            steps {
+                 script {
+                    catchError(buildResult: 'SUCCESS') {
+                        sh "ansible-lint ${playbook_path} > Report-ansible-lint.txt"
+                     }
+                 }
+            }
+        }
+}
