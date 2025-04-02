c2666364510b13d27e9e9db158b250ed48451a4c
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-02 14:07:45 +0530

    Update yamlLint.groovy

--- src/org/ansibleCI/yamlLint.groovy
+++ src/org/ansibleCI/yamlLint.groovy
@@ -2,11 +2,9 @@ package org.ansibleCI
 
 def call (String playbook_path) {
   stage('YAML Syntax Check') {
-          steps {
               script {
                   catchError(buildResult: 'SUCCESS') {
                       sh "yamllint ${playbook_path} > Report-yamllint.txt"
-                  }
               }
           }
       }
