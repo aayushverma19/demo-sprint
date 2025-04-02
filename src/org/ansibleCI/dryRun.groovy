d3b3ff4f9b3d764e210f0cd0afb6998306de5d4f
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-02 20:52:16 +0530

    Update dryRun.groovy

--- src/org/ansibleCI/dryRun.groovy
+++ src/org/ansibleCI/dryRun.groovy
@@ -5,9 +5,10 @@ def call (String inventory_path, String playbook_path, String key_path, String u
 
                 script {
                     catchError(buildResult: 'SUCCESS') {
-                        sh "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -C -D -i ${inventory_path} ${playbook_path} --private-key=${key_path} -u ${user_name}  > Report-dry-run.txt"
+                        sh "ansible-playbook -C -D -i ${inventory_path} ${playbook_path} --private-key=${key_path} -u ${user_name}  > Report-dry-run.txt"
                 }
             }
       }
   
   }
+//ANSIBLE_HOST_KEY_CHECKING=False 
