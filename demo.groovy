44066e7a3dc000f7c0e6e4cd302b2f61c74e9d0b
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-05 16:44:24 +0530

    Create demo.groovy

---
+++ demo.groovy
@@ -0,0 +1,51 @@
+folder('POC') {
+    displayName('POC')
+}
+folder('POC/CI-Checks') {
+    displayName('CI-Checks')
+}
+folder('POC/CI-Checks/Declarative') {
+    displayName('Declarative')
+}
+folder('POC/CI-Checks/Declarative/Generic') {
+    displayName('Generic')
+}
+
+pipelineJob('POC/CI-Checks/Declarative/Generic/cred-scanning') {
+    description("")
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/snaatak-Zero-Downtime-Crew/jenkins.git')
+                        credentials('git-cred') 
+                    }
+                    branch('*/main') 
+                }
+            }
+            scriptPath('CI/Generic/Declarative/Cred Scanning/Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('POC/CI-Checks/Declarative/Generic/License Scanning') {
+    description("Pipeline job for provisioning PostgreSQL EC2 instance in the Dev environment")
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/snaatak-Zero-Downtime-Crew/jenkins.git')
+                        credentials('git-cred') 
+                    }
+                    branch('*/Rohit-SCRUM-129') 
+                }
+            }
+            scriptPath('CI/Generic/Declarative/License Scanning')
+        }
+    }
+}
+
+
+
