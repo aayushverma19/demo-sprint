329eec3d27b4a984c9c722c4f6a8602d47c90f68
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-04-05 15:08:33 +0530

    Update add.groovy

--- add.groovy
+++ add.groovy
@@ -1,1 +1,99 @@
-
+folder('POC') {
+    displayName('POC')
+}
+folder('POC/CI-Checks') {
+    displayName('CI-Checks')
+}
+folder('POC/CI-Checks/Declarative') {
+    displayName('Declarative')
+}
+folder('POC/CI-Checks/Declarative/Generic') {
+    displayName('Generic')
+}
+folder('POC/CI-Checks/Declarative/Java') {
+    displayName('Java')
+}
+folder('POC/CI-Checks/Declarative/Python') {
+    displayName('Python')
+}
+folder('POC/CI-Checks/Declarative/Golang') {
+    displayName('Golang')
+}
+folder('POC/CI-Checks/Declarative/React') {
+    displayName('React')
+}
+folder('POC/CI-Checks/Declarative/Ansible') {
+    displayName('Ansible')
+}
+folder('POC/CI-Checks/Declarative/Ansible/Playbook') {
+    displayName('Playbook')
+}
+folder('POC/CI-Checks/Declarative/Terraform') {
+    displayName('Terraform')
+}
+folder('POC/CI-Checks/Declarative/Terraform/Module') {
+    displayName('Module')
+}
+folder('POC/CI-Checks/Scripted') {
+    displayName('Scripted')
+}
+folder('POC/CI-Checks/Scripted/Generic') {
+    displayName('Generic')
+}
+folder('POC/CI-Checks/Scripted/Java') {
+    displayName('Java')
+}
+folder('POC/CI-Checks/Scripted/Python') {
+    displayName('Python')
+}
+folder('POC/CI-Checks/Scripted/Golang') {
+    displayName('Golang')
+}
+folder('POC/CI-Checks/Scripted/React') {
+    displayName('React')
+}
+folder('POC/CI-Checks/Shared-Library') {
+    displayName('Shared-Library')
+}
+folder('POC/CI-Checks/Shared-Library/Generic') {
+    displayName('Generic')
+}
+folder('POC/CI-Checks/Shared-Library/Java') {
+    displayName('Java')
+}
+folder('POC/CI-Checks/Shared-Library/Python') {
+    displayName('Python')
+}
+folder('POC/CI-Checks/Shared-Library/Golang') {
+    displayName('Golang')
+}
+folder('POC/CI-Checks/Shared-Library/React') {
+    displayName('React')
+}
+folder('POC/CI-Checks/Shared-Library/Ansible') {
+    displayName('Ansible')
+}
+folder('POC/CI-Checks/Shared-Library/Ansible/Playbook') {
+    displayName('Playbook')
+}
+folder('POC/CI-Checks/Shared-Library/Ansible/Role') {
+    displayName('Role')
+}
+folder('POC/CD') {
+    displayName('CD')
+}
+folder('POC/CD/Ansible') {
+    displayName('Ansible')
+}
+folder('POC/CD/Ansible/Role') {
+    displayName('Role')
+}
+folder('POC/CD/Ansible/Playbook') {
+    displayName('Playbook')
+}
+folder('POC/CD/Terraform') {
+    displayName('Terraform')
+}
+folder('POC/CD/Terraform/Module') {
+    displayName('Module')
+}
